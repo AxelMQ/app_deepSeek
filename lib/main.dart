94549d4d612226d953679144932be04ccf8e8a7d
TuNombreDeUsuario <TuCorreo@ejemplo.com>
2025-03-10 23:55:46 -0400

    theme_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:app_deepseek/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,15 +10,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
+      title: 'DeepSeek API',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.light,
         ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+        textTheme: GoogleFonts.dosisTextTheme(),
       ),
+      darkTheme: ThemeData(
+        useMaterial3: true, 
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue, 
+          brightness: Brightness.dark,
+        ),
+        textTheme: GoogleFonts.dosisTextTheme(),
+      ),
+      themeMode: ThemeMode.light,
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
     );
   }
 }
